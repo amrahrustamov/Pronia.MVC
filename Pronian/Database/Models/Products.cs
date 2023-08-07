9cb31203bedbb8dd8207f3f177100c8649ac6fd4
Amrah <Amrah@DESKTOP-U9LPM9Q>
2023-08-07 13:54:11 +0400

    products info coming from database

---
+++ Pronian/Database/Models/Products.cs
@@ -0,0 +1,25 @@
+﻿namespace Pronian.Database.Models
+{
+    public class Products
+    {
+        public Products(string name, string description, string uRL, string color, string size, decimal price, int productOrder)
+        {
+            Name = name;
+            Description = description;
+            URL = uRL;
+            Color = color;
+            Size = size;
+            Price = price;
+            ProductOrder = productOrder;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string URL { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public decimal Price { get; set; }
+        public int ProductOrder { get;}
+
+    }
+}
---
+++ Pronian/Database/Repositories/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using Pronian.Database.Models;
+
+namespace Pronian.Database.Repositories
+{
+    public class ProductRepository
+    {
+        public static List<Products> products = new List<Products>();
+
+        static ProductRepository()
+        {
+            products.Add(new Products("American Marigold", "description area", "https://www.example.com", "Light green", "Medium size", 23,1));
+            products.Add(new Products("American Marigold", "description area", "https://www.example.com", "Light green", "Medium size", 23, 2));    
+            products.Add(new Products("American Marigold", "description area", "https://www.example.com", "Light green", "Medium size", 23, 3));
+            products.Add(new Products("American Marigold", "description area", "https://www.example.com", "Light green", "Medium size", 23, 4));
+            products.Add(new Products("American Marigold", "description area", "https://www.example.com", "Light green", "Medium size", 23, 5));
+            products.Add(new Products("American Marigold", "description area", "https://www.example.com", "Light green", "Medium size", 23, 6));
+            products.Add(new Products("American Marigold", "description area", "https://www.example.com", "Light green", "Medium size", 23, 7));
+            products.Add(new Products("American Marigold", "description area", "https://www.example.com", "Light green", "Medium size", 23, 8));
+
+        }
+        public List<Products> GetAll()
+        {
+            return products;
+        }
+    }
+}
